deb5c2c8e2804445d63f3a34604eeb9773e21b76
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-08-08 11:59:47 +0530

    Adding User Registration and Password encryption functionality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,6 +17,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//encrypting pw before we save, using middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  //generating salt for hashing (10 rounds)
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 //using router.js which provides an API for handling routes
 
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/userControllers.js';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userControllers.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
+router.post('/', registerUser);
 router.post('/login', authUser);
 router.route('/profile').get(authMiddleware, getUserProfile);
 
