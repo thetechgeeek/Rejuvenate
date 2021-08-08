d3efc8acc1d61033f420a0b68a797f40c28e2dca
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-08-08 09:35:11 +0530

    Adding the authentication middleware for user login.
    Middleware gets the token, verifies it and returns user data to user profile page.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+//script/middleware for validating the
+//token/authenticating the logging in user
+
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      //grabbing token from req.headers
+      //separating token from "Bearer"
+      token = req.headers.authorization.split(' ')[1];
+      //jwt.verify takes in token and secret
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('Not Authorized, token failed.');
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token.');
+  }
+});
+export { authMiddleware };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 //using router.js which provides an API for handling routes
 
 import express from 'express';
-import { authUser } from '../controllers/userControllers.js';
+import { authUser, getUserProfile } from '../controllers/userControllers.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/login', authUser);
+router.route('/profile').get(authMiddleware, getUserProfile);
 
 export default router;
