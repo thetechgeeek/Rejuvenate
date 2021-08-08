fa9ecc831d00c0f47e3c7a7240461d81aa9869e4
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-08-08 12:17:05 +0530

    Adding User Login Reducer & Action

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export const userActions_login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: 'USER_LOGIN_REQUEST',
+    });
+
+    //config for sending data, we want to send
+    //headers with content type/ will also
+    //set authorization for the token
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
+
+    dispatch({ type: 'USER_LOGIN_SUCCESS', payload: data });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: 'USER_LOGIN_FAIL',
+      payload:
+        //trying to getting the message obj from the custom error msg we
+        //created, if !exists, display the current msg
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
+import { userLoginReducer } from './reducers/userReducers';
 import { cartReducer } from './reducers/cartReducers';
 
 //combines the reducers in the store
@@ -15,14 +16,21 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : [];
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
+
 //keeping all the middlewares in one array 'middleware'
 const middleware = [thunk];
 //creating a store in reducers, initialState and middleware array
