bfe89e961701ce186f89e86a250390d6561c8c4a
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-08-17 10:34:19 +0530

    Updating order to paid in backend.

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -55,4 +55,27 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+//@desc  Update order to paid
+//@route  GET /api/orders/:id/pay
+//@access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer_email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,11 +4,13 @@ import express from 'express';
 import {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } from '../controllers/orderControllers.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').post(authMiddleware, addOrderItems);
 router.route('/:id').get(authMiddleware, getOrderById);
+router.route('/:id/pay').put(authMiddleware, updateOrderToPaid);
 
 export default router;
