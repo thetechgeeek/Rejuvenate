a036d99b82c677ba354624e91911a8b5fdf8c62f
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-08-17 07:58:51 +0530

    Fetching Order from database.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,14 @@
 //using router.js which provides an API for handling routes
 
 import express from 'express';
-import { addOrderItems } from '../controllers/orderControllers.js';
+import {
+  addOrderItems,
+  getOrderById,
+} from '../controllers/orderControllers.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').post(authMiddleware, addOrderItems);
+router.route('/:id').get(authMiddleware, getOrderById);
 
 export default router;
