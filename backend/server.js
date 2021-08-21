ac0f9af765abb1944eab080e414c6935133e0b02
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-08-21 10:44:12 +0530

    Fixed bugs.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
+import path from 'path';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
