984760951fc7dbdfb5f7f646aa4546ff13f17f9d
Rupesh Ojha <bhs.rupesh@gmail.com>
2021-08-26 22:22:03 +0530

    Adding Carousal.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { productActions_list } from '../actions/productActions';
-import { Container, Image, Row, Col } from 'react-bootstrap';
+import { Carousal, Container, Image, Row, Col } from 'react-bootstrap';
 import Message from '../components/message';
 import Loader from '../components/loader';
 import Paginate from '../components/Paginate';
@@ -19,6 +19,12 @@ const HomeScreen = ({ match }) => {
   const productList = useSelector((state) => state.productList);
   //extracting info from 'productList' part of state into an Obj
   const { loading, error, products, page, pages } = productList;
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
   //dispatching 'list products' action using useEffect
   useEffect(() => {
@@ -63,6 +69,47 @@ const HomeScreen = ({ match }) => {
           </Col>
         </Row>
       </Container>
+
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='holder.js/800x400?text=First slide&bg=373940'
+            alt='First slide'
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='holder.js/800x400?text=Second slide&bg=282c34'
+            alt='Second slide'
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='holder.js/800x400?text=Third slide&bg=20232a'
+            alt='Third slide'
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
       <h3
         style={{
           textAlign: 'center',
